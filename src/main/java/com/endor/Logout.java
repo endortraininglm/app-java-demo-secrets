9af961a8f3edb4ca21731b7035fb358d1afbcc18
endor-lmoreno <lmoreno@endor.ai>
2026-02-05 08:41:58 -0500

    intial - commit leaked secret in main

---
+++ src/main/java/com/endor/Logout.java
@@ -0,0 +1,23 @@
+package com.endor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet("/clothing-shop/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private static final String githubTokenLogout = "github_pat_55LOGA0K3G6QVAyNaCS_GtRQFBQzyKSvkcpKAeOgn3S9vW0D6CuCls5AnVyuBS97D3V2ILTtu2tmWS5";
+
+	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if(session != null){
+            session.invalidate();
+        }
+        response.sendRedirect("login");
+	}
+}
