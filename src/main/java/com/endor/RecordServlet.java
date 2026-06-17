58b0651f64a0e23b85b564bcdce9ddfaa28a2c85
endor-lmoreno <lmoreno@endor.ai>
2026-06-17 10:47:03 -0500

    some secret

--- src/main/java/com/endor/RecordServlet.java
+++ src/main/java/com/endor/RecordServlet.java
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/clothing-shop/RecordServlet")
 public class RecordServlet extends HttpServlet {
+    private static String SECRET = "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";
     static String connectionUrl = "";
     static String dbUser = "";
     static String dbPassword = "";
